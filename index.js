151db2ce59f82ea87c2050843baab32c474c1838
SaifullaJubair <saifullajubair6@gmail.com>
2022-12-30 21:27:50 +0600

    jwt token and admin verify

--- index.js
+++ index.js
@@ -2,14 +2,38 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
+//middleware
 app.use(cors())
 app.use(express.json())
 
+const { query } = require('express');
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dnl5op5.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+//JWT Token
+function verifyJWT(req, res, next) {
+   // console.log('token verifyJWT', req.headers.authorization)
+   const authHeader = req.headers.authorization;
+   if (!authHeader) {
+      return res.status(401).send('unauthorized access')
+   }
+   const token = authHeader.split(' ')[1];
+   // console.log(token)
+
+   jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+      if (err) {
+         console.log(err);
+         return res.status(403).send({ message: 'forbidden access' })
+      }
+      req.decoded = decoded
+      next()
+   })
+}
 
 async function run() {
 
@@ -32,25 +56,51 @@ async function run() {
 
       //Saifullah write code here
 
+      const verifyAdmin = async (req, res, next) => {
+         // console.log("inside verifyAdmin", req.decoded.email);
+         const decodedEmail = req.decoded.email;
+         const query = { email: decodedEmail }
+         const user = await usersCollection.findOne(query);
+
+         if (user?.role !== 'admin') {
+            return res.status(403).send({ message: 'forbidden access' })
+         }
+         next()
+      }
+
+      app.get('/jwt', async (req, res) => {
+         const email = req.query.email;
+         // console.log(email);
+         const query = { email: email };
+         const user = await usersCollection.findOne(query);
+         if (user) {
+            const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '24h' })
+            // console.log(token)
+            return res.send({ accessToken: token })
+         }
+         res.status(403).send({ accessToken: '' })
+
+      })
+
       app.get('/latestnews', async (req, res) => {
 
          const result = await (newsesCollection.find({}).limit(6)).toArray();
          res.send(result);
       })
-      app.get('/users', async (req, res) => {
+      app.get('/users', verifyJWT, verifyAdmin, async (req, res) => {
          const query = {}
          const users = await usersCollection.find(query).toArray()
          res.send(users)
       })
 
-      app.get('/users/:id', async (req, res) => {
+      app.get('/users/:id', verifyJWT, verifyAdmin, async (req, res) => {
          const id = req.params.id
          const query = { _id: ObjectId(id) }
          const user = await usersCollection.findOne(query)
          res.send(user)
       })
 
-      app.put('/users/update/:id', async (req, res) => {
+      app.put('/users/update/:id', verifyJWT, verifyAdmin, async (req, res) => {
          const id = req.params.id;
          const filter = { _id: ObjectId(id) }
          const option = { upsert: true }
@@ -63,7 +113,7 @@ async function run() {
          res.send(result)
       })
 
-      app.delete('/users/:id', async (req, res) => {
+      app.delete('/users/:id', verifyJWT, verifyAdmin, async (req, res) => {
          const id = req.params.id;
          const filter = { _id: ObjectId(id) }
          const result = await usersCollection.deleteOne(filter)
@@ -110,9 +160,9 @@ async function run() {
          res.send(result);
       });
 
-      app.get('/comment/:id', async (req, res) => {
+      app.get('/comment/:id', verifyJWT, async (req, res) => {
          const id = req.params.id;
-     
+
          const query = { newsId: id };
          const cursor = commentCollection.find(query).limit(10);
          const news = await cursor.sort({ createdAt: -1 }).toArray();
@@ -186,7 +236,7 @@ async function run() {
          res.send(result)
       })
 
-      app.put('/editnews/:id', async (req, res) => {
+      app.put('/editnews/:id', verifyJWT, verifyAdmin, async (req, res) => {
          const id = req.params.id;
          const data = req.body;
          const filter = { _id: ObjectId(id) }
@@ -204,12 +254,12 @@ async function run() {
          res.send(result)
       })
 
-      app.delete('/news/:id', async (req, res) => {
+      app.delete('/news/:id', verifyJWT, verifyAdmin, async (req, res) => {
          const id = req.params.id;
          const result = await newsesCollection.deleteOne({ _id: ObjectId(id) });
          res.send(result)
       })
-      app.delete('/deletecategory', async (req, res) => {
+      app.delete('/deletecategory', verifyJWT, verifyAdmin, async (req, res) => {
          const id = req.body._id;
          const result = await categoriesCollection.deleteOne({ _id: ObjectId(id) });
          res.send(result)
